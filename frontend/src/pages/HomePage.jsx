1beb8f70cf213fd702373d4116ba1ca469643043
Parthav <parthavsabrwal@gmail.com>
2025-02-10 12:19:48 +0530

    latest

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,7 +9,6 @@ function HomePage() {
     <Navbar />
     <Carousel />
     <Footer />
-      {/* <div className="h-screen w-screen bg-no-repeat bg-center bg-cover" style={{backgroundImage: "url(https://miro.medium.com/v2/resize:fit:1400/1*WoEu5XEHEwYFcDkbl1A3Bw.jpeg)"}}></div> */}
     </>
   );
 }
